da8a447a0f260c450ed0e8c2dd71552b3fb3f380
Kenta Sato <bicycle1885@gmail.com>
2017-10-18 02:49:22 +0900

    add deprecations

--- src/CodecBzip2.jl
+++ src/CodecBzip2.jl
@@ -20,4 +20,10 @@ include("libbz2.jl")
 include("compression.jl")
 include("decompression.jl")
 
+# Deprecations
+@deprecate Bzip2Compression         Bzip2Decompressor
+@deprecate Bzip2CompressionStream   Bzip2CompressorStream
+@deprecate Bzip2Decompression       Bzip2Decompressor
+@deprecate Bzip2DecompressionStream Bzip2DecompressorStream
+
 end # module
