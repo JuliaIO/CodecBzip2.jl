5639a24f02a606c32d1d399820a5960d8801f8f7
Kenta Sato <bicycle1885@gmail.com>
2018-08-12 13:43:14 +0900

    remove deprecated constructors and clean up code (#9)

--- src/CodecBzip2.jl
+++ src/CodecBzip2.jl
@@ -1,5 +1,3 @@
-__precompile__()
-
 module CodecBzip2
 
 export
@@ -14,30 +12,11 @@ import TranscodingStreams:
     Memory,
     Error,
     initialize,
-    finalize
-
-# TODO: This method will be added in the next version of TranscodingStreams.jl.
-function splitkwargs(kwargs, keys)
-    hits = []
-    others = []
-    for kwarg in kwargs
-        push!(kwarg[1] ∈ keys ? hits : others, kwarg)
-    end
-    return hits, others
-end
-
-if !isdefined(Base, :Cvoid)
-    const Cvoid = Void
-end
+    finalize,
+    splitkwargs
 
 include("libbz2.jl")
 include("compression.jl")
 include("decompression.jl")
-
-# Deprecations
-@deprecate Bzip2Compression         Bzip2Compressor
-@deprecate Bzip2CompressionStream   Bzip2CompressorStream
-@deprecate Bzip2Decompression       Bzip2Decompressor
-@deprecate Bzip2DecompressionStream Bzip2DecompressorStream
 
 end # module
