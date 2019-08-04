25cb40f3873bc576d2e2d487fb99cf56273ba4ea
Kenta Sato <bicycle1885@gmail.com>
2019-08-04 17:22:59 +0900

    use pre-built binaries and drop Julia 0.7 (#12)

--- src/CodecBzip2.jl
+++ src/CodecBzip2.jl
@@ -14,6 +14,14 @@ import TranscodingStreams:
     initialize,
     finalize,
     splitkwargs
+using Libdl
+
+const libbz2path = joinpath(dirname(@__FILE__), "..", "deps", "deps.jl")
+if !isfile(libbz2path)
+    error("CodecBzip2.jl is not installed properly, run Pkg.build(\"CodecBzip2\") and restart Julia.")
+end
+include(libbz2path)
+check_deps()
 
 include("libbz2.jl")
 include("compression.jl")
